8f0006d9355a53dd0b9e1605de399324fdc49fb1
Nasser <nasserbrid@gmail.com>
2024-06-06 09:29:22 +0200

    Création du projet sprin-boot-web

---
+++ src/test/java/fr/groupe2/tp/TpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.groupe2.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
