e15dc15cdb9bb7bdc031b5259bb18db98887b89e
Ouerdani Sakina <ouerdanisakina@gmail.com>
2024-06-06 16:50:27 +0200

    dernier modification de dao

--- src/main/java/fr/groupe2/tp/dao/OrdershopDao.java
+++ src/main/java/fr/groupe2/tp/dao/OrdershopDao.java
@@ -1,5 +1,7 @@
 package fr.groupe2.tp.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import fr.groupe2.tp.model.Ordershop;
 
 @Repository
 public interface OrderShopDao extends CrudRepository<Ordershop, Integer>{
+	
+	 public List<Ordershop> getByOrderStatus(String orderStatus);
 	
 }
--- src/main/java/fr/groupe2/tp/dao/OrdershopitemDao.java
+++ src/main/java/fr/groupe2/tp/dao/OrdershopitemDao.java
@@ -1,10 +1,21 @@
 package fr.groupe2.tp.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.groupe2.tp.model.Ordershopitem;
 
 public interface  OrdershopitemDao  extends CrudRepository<Ordershopitem, Integer> { 
+	
+	public List<Ordershopitem> getByArticleId(Integer Id_article);
+	
+	public List<Ordershopitem> getByOrderId(Integer orderId);
+	
+	public List<Ordershopitem> getByUserId(Integer userId);
+	
+	
+    
 	
 	
 }
